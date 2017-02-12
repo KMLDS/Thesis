764c212d2d342b8474244fd250188f618d6ae845
Kevin Lyons <kevin.lyons.physics@gmail.com>
2017-02-12 17:34:30 -0500

    bib fixed, numbering added

---
+++ Intro/auto/intro.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook
+ "intro"
+ (lambda ()
+   (LaTeX-add-labels
+    "eq:postselection state"
+    "eq:likelihood function"))
+ :latex)
+
---
+++ SqueezedLight/auto/squeezed.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "squeezed"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:setup"
+    "eq:displacementop"
+    "eq:generalvariance"
+    "eq:squeezedsnr"
+    "eq:darkannihilation"
+    "fig:squeezedsnrgain"
+    "eq:wvsignal"))
+ :latex)
+
